09d73c02d077ca043a77da49c1b30febf84dc836
Zisan-uzum <uzummzisann@gmail.com>
2022-06-08 13:24:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 class Formatter {
-  //add static methods here
+    static capitalize(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    static sanitize(string) {
+        return string.replace(/[^A-Za-z0-9 '-]/g, ""); // ^ means not
+    }
+    static titleize(string) {
+        const exception = [
+            "a",
+            "an",
+            "the",
+            "but",
+            "of",
+            "and",
+            "for",
+            "at",
+            "by",
+            "from",
+        ];
+
+        return string
+            .split(" ")
+            .map((word, index) => {
+                if (index === 0 || !exception.includes(word)) {
+                    return this.capitalize(word);
+                } else {
+                    return word;
+                }
+            })
+            .join(" ");
+    }
 }
